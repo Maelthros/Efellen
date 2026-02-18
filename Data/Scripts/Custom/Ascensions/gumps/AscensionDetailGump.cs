ea8eda019e17a5994026c726c800fdb5fd1014eb
EstelMatiazi <estelmatiazi@gmail.com>
2026-02-18 15:39:28 -0300

    improves the datail gump

--- Data/Scripts/Custom/Ascensions/gumps/AscensionDetailGump.cs
+++ Data/Scripts/Custom/Ascensions/gumps/AscensionDetailGump.cs
@@ -2,113 +2,123 @@ using System;
 using Server;
 using Server.Gumps;
 using Server.Mobiles;
+using Server.Network;
 using Server.Custom.Ascensions;
-
-namespace Server.Custom.Ascensions.Gumps
+namespace Server.Custom.Ascensions
 {
     public class AscensionDetailGump : Gump
     {
-        private PlayerMobile m_Player;
+        private PlayerMobile m_From;
+        private string m_Key;
+        private AscensionProgress m_Progress;
+
+        private const int Button_Unlock = 1;
+        private const int Button_LevelUp = 2;
+        private const int Button_Back = 3;
+
         private AscensionType m_Type;
 
-        public AscensionDetailGump(PlayerMobile pm, AscensionType type) : base(120, 80)
+        public AscensionDetailGump(PlayerMobile from, AscensionType type): base(100, 100)
         {
-            m_Player = pm;
+            m_From = from;
             m_Type = type;
 
-            AscensionProgress prog = pm.AscensionProfile.Get(type);
+            m_Progress = from.AscensionProfile.Get(type);
 
+            Closable = true;
+            Disposable = true;
+            Dragable = true;
+            Resizable = false;
+
+            BuildGump();
+        }
+
+        private void BuildGump()
+        {
             AddPage(0);
-            AddBackground(0, 0, 520, 450, 9270);
 
-            AddLabel(180, 15, 1152, type.ToString());
+            AddBackground(0, 0, 520, 610, 9270);
+            AddAlphaRegion(10, 10, 510, 600);
 
-            AddLabel(30, 60, 1153, "Level: " + prog.Level);
+            AddLabel(140, 20, 1152, m_Type.ToString());
 
-            int requiredExp = prog.GetRequiredExperience();
+            AddLabel(30, 85, 88, "Level:");
+            AddLabel(200, 85, 1152, m_Progress.Level.ToString());
 
-            if (!prog.IsMaxLevel())
+            AddLabel(30, 110, 88, "Experience:");
+            AddLabel(200, 110, 1152, m_Progress.Experience.ToString());
+
+            int required = m_Progress.GetRequiredExperience();
+
+            AddLabel(30, 135, 88, "Next Level XP:");
+            AddLabel(200, 135, 1152, required.ToString());
+
+            AddLabel(30, 220, 1152, "Benefits:");
+            AddHtml(30, 250, 460, 80, AscensionDefinitions.GetDescription(m_Type), true, true);
+
+            AddLabel(30, 360, 1152, "Granted Abilities:");
+            AddHtml(30, 380, 460, 140, AscensionDefinitions.GetAbilities(m_Type), true, true);
+
+            int bottomY = 560;
+
+            if (m_From.AscensionProfile.ActiveAscension == m_Type)
             {
-                AddLabel(30, 85, 1153,
-                    "Experience: " + prog.Experience + " / " + requiredExp);
-            }
-            else
-            {
-                AddLabel(30, 85, 68, "Maximum Level Reached");
+                AddLabel(320, bottomY, 63, "Currently Active");
             }
 
-
-            // Fake progress bar (visual only)
-            double percent = (double)prog.Experience / requiredExp;
-            int width = (int)(300 * percent);
-
-            AddBackground(30, 110, 300, 20, 9200);
-            AddBackground(30, 110, width, 20, 9300);
-
-            // Benefits
-            AddLabel(30, 150, 1152, "Benefits:");
-            AddHtml(30, 170, 460, 80, GetBenefits(type), true, true);
-
-            // Abilities
-            AddLabel(30, 260, 1152, "Granted Abilities:");
-            AddHtml(30, 280, 460, 80, GetAbilities(type), true, true);
+            AddButton(40, bottomY, 4014, 4016, Button_Back, GumpButtonType.Reply, 0);
+            AddLabel(75, bottomY, 1152, "Back");
 
             // Upgrade section
-            if (prog.CanLevelUp())
+            if (m_Progress.Level == 0 || m_Progress.CanLevelUp())
             {
-                int next = prog.GetNextLevel();
+                int next = m_Progress.GetNextLevel();
                 int gold = AscensionCosts.GetGoldCost(next);
                 int dust = AscensionCosts.GetDustCost(next);
                 int scrolls = AscensionCosts.GetScrollCost(next);
-            
-                AddLabel(350, 85, 1153,
-                "Cost: " + gold + "g / " +
-                dust + " dust / " +
-                scrolls + " scrolls");
-            
-                AddButton(350, 110, 4005, 4007, 3000 + (int)type, GumpButtonType.Reply, 0);
-                AddLabel(385, 110, 68, "Upgrade");
+
+                int upgradeY = 510;
+
+                AddLabel(260, upgradeY, 1153,
+                    "Cost: " + gold + "g / " +
+                    dust + " dust / " +
+                    scrolls + " scrolls");
+
+                AddButton(260, upgradeY + 25, 4005, 4007, 3000 + (int)m_Type, GumpButtonType.Reply, 0);
+                AddLabel(295, upgradeY + 25, 68, "Upgrade");
             }
-            if (prog.Level > 0)
+
+            if (m_Progress.Level > 0)
             {
-                AddButton(40, 290, 4005, 4007, 2, GumpButtonType.Reply, 0);
-                AddLabel(75, 290, 1152, "Activate Ascension");
+                AddButton(320, bottomY, 4005, 4007, 2, GumpButtonType.Reply, 0);
+                AddLabel(355, bottomY, 1152, "Activate Ascension");
             }
-
-
-            AddButton(200, 400, 4017, 4019, 1, GumpButtonType.Reply, 0);
-            AddLabel(235, 400, 1152, "Back");
         }
-
-        public override void OnResponse(Server.Network.NetState sender, RelayInfo info)
+        public override void OnResponse(NetState sender, RelayInfo info)
         {
-            if (info.ButtonID == 1)
+            switch (info.ButtonID)
             {
-                m_Player.SendGump(new AscensionSelectionGump(m_Player));
-                return;
-            }
-            if (info.ButtonID == 2)
-            {
-                m_Player.ActivateAscension(m_Type);
-                m_Player.SendGump(new AscensionDetailGump(m_Player, m_Type));
-            }
+                case Button_Back:
+                    m_From.SendGump(new AscensionSelectionGump(m_From));
+                    return;
 
+                case 2: // Activate
+                    m_From.ActivateAscension(m_Type);
+                    Refresh();
+                    return;
+            }
 
             if (info.ButtonID >= 3000)
             {
                 AscensionType type = (AscensionType)(info.ButtonID - 3000);
-                m_Player.SendGump(new AscensionUpgradeConfirmGump(m_Player, type));
+                m_From.SendGump(new AscensionUpgradeConfirmGump(m_From, type));
             }
         }
 
-        private string GetBenefits(AscensionType type)
-        {
-            return AscensionDefinitions.GetBenefits(type);
-        }
 
-        private string GetAbilities(AscensionType type)
+        private void Refresh()
         {
-            return AscensionDefinitions.GetAbilities(type);
+            m_From.SendGump(new AscensionDetailGump(m_From, m_Type));
         }
     }
 }
