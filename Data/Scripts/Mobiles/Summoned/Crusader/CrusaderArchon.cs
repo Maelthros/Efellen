1324b392346118745f3528a38b21fa954a93fa8a
EstelMatiazi <estelmatiazi@gmail.com>
2026-02-24 14:57:51 -0300

    prevents archon from ninja looting

--- Data/Scripts/Mobiles/Summoned/Crusader/CrusaderArchon.cs
+++ Data/Scripts/Mobiles/Summoned/Crusader/CrusaderArchon.cs
@@ -63,7 +63,7 @@ namespace Server.Mobiles
 
 		public override Poison PoisonImmune { get { return Poison.Regular; } }
 
-		public override bool CanRummageCorpses{ get{ return true; } }
+		public override bool CanRummageCorpses{ get{ return false; } }
 
 		public CrusaderArchon( Serial serial ): base( serial )
 		{
