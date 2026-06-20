f86093230f13403d5e3fad12b1747fff0a892d2f
EstelMatiazi <estelmatiazi@gmail.com>
2026-06-20 10:47:20 -0300

    fix typo

--- Data/Scripts/Custom/marks/Items/Marks/MarksOfTheShadowbroker.cs
+++ Data/Scripts/Custom/marks/Items/Marks/MarksOfTheShadowbroker.cs
@@ -10,7 +10,7 @@ namespace Server.Items
         {
         }
         
-        public override string DefaultDescription{ get{ return "A Mark of the Shadowbroker represents your prowess as a thief. It can be aqquired by burglars as they adventure and filfer the pockets of their victims. The guildmaster of the thieves guild can offer many trinkets for those that would speak of rewards with them."; } }
+        public override string DefaultDescription{ get{ return "A Mark of the Shadowbroker represents your prowess as a thief. It can be aqquired by burglars as they adventure and pilfer the pockets of their victims. The guildmaster of the thieves guild can offer many trinkets for those that would speak of rewards with them."; } }
 
         [Constructable]
         public MarksOfTheShadowbroker(int amount) : base(0x2ff8)
